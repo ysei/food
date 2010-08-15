98e3889abaeaea5104af3ec91a648d9598dd4033
ota <ota.pavelek@gmail.com>
2010-08-15 18:30:18 +0200

    ukladani konfigurace

---
+++ src/config.cpp
@@ -0,0 +1,86 @@
+#include "config.h"
+
+Configuration::Configuration() : configurationFileName("config.xml"), x(0), y(0), width(400), height(300) {
+}
+
+bool Configuration::LoadConfiguration() {
+	QFile file(configurationFileName);
+
+	if (!file.open(QIODevice::ReadOnly | QIODevice::Text)) {
+		return false;
+	}
+
+	
+	QXmlStreamReader xml(&file);
+	QXmlStreamReader::TokenType token;
+
+	do {
+		token = xml.readNext();
+		if (token == QXmlStreamReader::StartElement) {
+			QString elementName = xml.name().toString();
+			token = xml.readNext();
+			qDebug() << elementName;
+			if (elementName == "windowGeometry") {
+				QString readValue = xml.text().toString();
+				if (!readValue.isEmpty()) {
+					QStringList tmpStringList;
+					tmpStringList = readValue.split(" ");
+					x = tmpStringList[0].toInt();
+					y = tmpStringList[1].toInt();
+					width = tmpStringList[2].toInt();
+					height = tmpStringList[3].toInt();
+				}
+			}
+			if (elementName == "foodDB") {
+				DBFileName = xml.text().toString();
+			}
+		}
+	} while (!(xml.atEnd()));
+
+	qDebug() << x << y << width << height;
+	qDebug() << DBFileName;
+
+	file.close();
+	return true;
+}
+
+bool Configuration::SaveConfiguration() {
+	QFile file(configurationFileName);
+
+	if (!file.open(QIODevice::WriteOnly)) {
+		return false;
+	}
+
+	QXmlStreamWriter stream(&file);
+	stream.setAutoFormatting(true);
+	stream.writeStartDocument();
+	stream.writeStartElement("configuration");
+		QString windowGeometry;
+		windowGeometry = QString::number(x) + " " + QString::number(y) + " " + QString::number(width) + " " + QString::number(height);
+		stream.writeTextElement("windowGeometry", windowGeometry);
+		//stream.writeTextElement("windowGeometry", QString(QString::number(x)));
+		stream.writeTextElement("foodDB", DBFileName);
+	stream.writeEndElement();
+	stream.writeEndDocument();
+
+	file.close();
+	return true;
+}
+
+QString Configuration::GetDBFileName() {
+	return DBFileName;
+}
+
+void Configuration::GetWindowGeometry(int &_x, int &_y, int &_width, int &_height) {
+	_x = x;
+	_y = y;
+	_width = width;
+	_height = height;
+}
+
+void Configuration::SetWindowGeometry(int _x, int _y, int _width, int _height) {
+	x = _x;
+	y = _y;
+	width = _width;
+	height = _height;
+}
---
+++ src/config.h
@@ -0,0 +1,30 @@
+#ifndef __CONFIG_H__
+#define __CONFIG_H__
+
+#include <QXmlStreamReader>
+#include <QXmlStreamWriter>
+#include <QFile>
+#include <QString>
+#include <QStringList>
+
+#include <QDebug>
+
+class Configuration {
+	public:
+		Configuration();
+		QString GetDBFileName();
+		bool LoadConfiguration();
+		bool SaveConfiguration();
+		void GetWindowGeometry(int &_x, int &_y, int &_width, int &_height);
+		void SetWindowGeometry(int _x, int _y, int _width, int _height);
+
+	private:
+		QString configurationFileName;
+		QString DBFileName;
+		int x;
+		int y;
+		int width;
+		int height;
+};
+
+#endif
